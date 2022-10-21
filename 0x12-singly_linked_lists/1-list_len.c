dd7d38293c75977d8f1c06d6fb7d1dcea2dc8ac7
Francis <francismensah11111@gmail.com>
2022-10-20 18:44:27 -0700

    Bug fixed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,18 @@
 #include "lists.h"
-
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
- * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
- *
- * Return: The number of elements in h.
+ * listint_len - length of linked list
+ * @h: struct to go through
+ * Return: amount of elements
  */
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
-	size_t elements = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		elements++;
+		count++;
 		h = h->next;
 	}
 
-	return (elements);
+	return (count);
 }
